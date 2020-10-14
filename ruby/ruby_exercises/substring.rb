e25f04517942888bcce148221652eb7892a31f8a
stivencabrera3 <stiven.cabrera3@gmail.com>
2020-10-13 23:57:15 -0400

    complete substring exercise

---
+++ ruby/ruby_exercises/substring.rb
@@ -0,0 +1,19 @@
+def substrings(str,arr)
+    str_hash = Hash.new(0)
+    str = str.downcase
+
+    arr.each do |word|
+        if str.include?(word)
+            str_hash[word]+= str.scan(word).length
+        else
+            next
+        end
+    end
+    return str_hash
+end
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+print substrings("Howdy partner, sit down! How's it going?", dictionary)
+
