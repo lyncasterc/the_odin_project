a32ffcfbde1e3744370179a1f1a8c6dce9f06900
stivencabrera3 <stiven.cabrera3@gmail.com>
2020-10-25 03:36:21 -0400

    start hangman project

--- ruby/IO/event_manager/lib/event_manager.rb
+++ ruby/IO/event_manager/lib/event_manager.rb
@@ -31,6 +31,12 @@ end
 # Instead of the code above, Ruby comes with tools to handle parsing CSV
 
 require "csv"
+require "google/apis/civicinfo_v2"
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw' 
+
 
 
 def clean_zipcode(zipcode)
@@ -52,8 +58,22 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators = legislators.officials
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+
+  legislator_names = legislators.map do |legislator|
+    legislator.name
+  end
   
-  puts "#{name}, #{zipcode}"
+  puts "#{name}, #{zipcode} #{legislator_names}"
   
 end
 
---
+++ ruby/IO/hangman/hangman.rb
@@ -0,0 +1,3 @@
+# def pick_random_line
+#   File.readlines("data.txt").sample
+# end
