f360b31cbd9c1a4659114be0006a3f81ab2f8b96
stivencabrera3 <stiven.cabrera3@gmail.com>
2020-10-26 02:24:40 -0400

    create HangMan class, random_word method

--- ruby/IO/hangman/hangman.rb
+++ ruby/IO/hangman/hangman.rb
@@ -1,3 +1,55 @@
+class HangMan
+
+  def initialize
+    @word = random_word
+  end
+
+  public
+  def main
+
+    current_game_state = {
+
+      :word => @word,
+
+      :turns_left => 5,
+
+      :guessed_letters => [],
+
+      :available_letters => ('a'..'z').to_a,
+
+    }
+
+    available_letters = current_game_state[:available_letters]
+
+
+  end
+
+  private
+  def random_word
+    words = File.readlines("dictionary.txt").select{|word| word.length >= 5 && word.length <= 12}.sample
+  end
+
+  def save_game(game_state)
+
+  end
+  
+end
+
+
+
+hangman = HangMan.new()
+hangman.main
+
+
+
+
+
 # def pick_random_line
 #   File.readlines("data.txt").sample
 # end
+
+# " ------|
+#   |     O
+#   |   --|--
+#   |    / \
+#   | _________ "
