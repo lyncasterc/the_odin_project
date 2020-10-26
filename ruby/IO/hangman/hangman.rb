78048c0183330205c2d632496acfe2d609d30a27
stivencabrera3 <stiven.cabrera3@gmail.com>
2020-10-26 18:44:17 -0400

    added menu, saving and loading game methods

--- ruby/IO/hangman/hangman.rb
+++ ruby/IO/hangman/hangman.rb
@@ -1,36 +1,95 @@
 class HangMan
 
   def initialize
-    @word = random_word
+    @word = nil
   end
 
   public
   def main
 
-    current_game_state = {
+    choice = get_menu_choice
 
-      :word => @word,
+    
 
-      :turns_left => 5,
+    case choice
+      when "1"
 
-      :guessed_letters => [],
+        @word = random_word
 
-      :available_letters => ('a'..'z').to_a,
+        current_game_state = {
 
-    }
+          :word => @word,
 
-    available_letters = current_game_state[:available_letters]
+          :turns_left => 5,
 
+          :guessed_letters => [],
+
+          :available_letters => ('a'..'z').to_a,
+        }
+
+      when "2"
+        currrent_game_state = load_saved_game
+      else
+        exit
+    end
+
+
+
+    # menu
+  
+    
 
   end
 
   private
   def random_word
-    words = File.readlines("dictionary.txt").select{|word| word.length >= 5 && word.length <= 12}.sample
+    words = File.readlines("dictionary.txt").select{|word| word.length >= 5 && word.length <= 12}.sample.chomp.downcase
   end
 
   def save_game(game_state)
+    require "yaml"
 
+    File.open("hangman_save.yaml", "w") do |file|
+      file.write YAML::dump(game_state)
+    end
+  end
+
+  def load_saved_game
+    require "yaml"
+    game_state = nil
+
+    File.open("hangman_save.yaml", "r") do |file|
+      game_state = YAML::load(file.read)
+    end
+
+    return game_state
+  end
+
+  def get_menu_choice
+    
+    if @word.nil?
+      puts "1. Play game\n2. Load saved game\n3. Quit"
+      print "Enter choice: "
+      choice = gets.chomp.to_i
+      unless (1..3).include?(choice)
+        puts "Invalid input"
+        puts "1. Play game\n2. Load saved game\n3. Quit"
+        print "Enter choice: "
+        choice = gets.chomp.to_i  
+      end
+      return choice
+    else
+      puts "1. New game\n2. Save game\n 3. Quit"
+      print "Enter choice: "
+      choice = gets.chomp.to_i
+      unless (1..3).include?(choice)
+        puts "Invalid input"
+        puts "1. Play game\n2. Load saved game\n3. Quit"
+        print "Enter choice: "
+        choice = gets.chomp.to_i  
+      end
+      return choice
+    end
   end
   
 end
