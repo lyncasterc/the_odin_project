a0711a1e0f8deea125fc8ef509576147602fd17f
stivencabrera3 <stiven.cabrera3@gmail.com>
2020-11-02 18:11:23 -0500

    solve recursion_project assignment1

--- ruby/recursion_project.rb
+++ ruby/recursion_project.rb
@@ -10,11 +10,22 @@ def fibs(n)
   return fib_nums
 end
 
-puts fibs(5)
+print fibs(5)
 
-def fibs_rec(n,)
+puts "\n\n"
+
+def fibs_rec(n,fib_nums=[0,1],i=-1)
+
+  i += 1
+
+  if fib_nums.length == n + 1
+    return fib_nums
+  else
+    fib_nums.push(fib_nums[i] + fib_nums[i+1])
+    fibs_rec(n,fib_nums,i)
+  end
+
   
-
 end
 
-print fibs_rec(6)
+print fibs_rec(5)
