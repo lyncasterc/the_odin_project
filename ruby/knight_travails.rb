de3bb2d1910890d6a544ebe38ec95c84dd9c2df3
stivencabrera3 <stiven.cabrera3@gmail.com>
2020-12-13 16:40:52 -0500

    first commit, create board class and knight class

--- ruby/knight_travails.rb
+++ ruby/knight_travails.rb
@@ -1,9 +1,54 @@
-class Knight 
-  def initialize(position = nil)
-    @position = [0,0] if position.nil?
+class Node
+
+end
+
+class Board
+  attr_accessor :board
+
+  def initialize
+    @board = self.create_board
   end
 
-  def possible_moves
-    
+  private
+  def create_board
+    a = Array.new(8){|i| i}
+    board = []
+
+    a.each do |x|
+      a.each do |y|
+        board.push([x,y])
+      end
+    end
+
+    return board
   end
 end
+
+class Knight
+  attr_reader :current_pos
+
+  def initialize(current_pos)
+    @current_pos = current_pos
+  end
+
+  private
+  def valid_move?(move)
+    x1 = @current_pos[0]
+    y1 = @current_pos[1]
+    x2 = move[0]
+    y2 = move[1]
+
+    return false if x2 > 7 || x2 < 0 || y2 > 7 || y2 < 0
+
+    if x2 - x1 == 2 && y2 - y1 == 1
+      return true
+    elsif y2 - y1 == 2 && x2 - x1 == 1
+      return true
+    else 
+      return false
+    end
+  end
+end
+
+chess = Board.new
+print chess.board
