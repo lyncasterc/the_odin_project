f6631ccab4f485fd50da9e36e1e21b746fd20f26
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-06-08 23:48:38 -0400

    start make_move method

--- ruby/connect_four/lib/connect_four.rb
+++ ruby/connect_four/lib/connect_four.rb
@@ -1,9 +1,26 @@
 class ConnectFour 
-  attr_reader :internal_board
-
+  attr_reader :internal_board, :game_board
   def initialize
     @internal_board = Array.new(42)
     @game_board = Array.new(7){Array.new(6,'O')}
+  end
+
+  public
+  def make_move(player, col)
+    index = col - 1
+    @game_board[index].reverse!
+
+    @game_board[index].each_index do |i|
+      if @game_board[index][i] == "O"
+
+        if player == 1 then @game_board[index][i] = "1" else @game_board[index][i] = "2" end
+
+        @game_board[index].reverse!
+
+        break
+      end
+    end
+
   end
 
   def display_board
@@ -15,10 +32,14 @@ class ConnectFour
     end
   end
 
-  
 
 end
 
-# game = ConnectFour.new
+game = ConnectFour.new
+game.make_move(1,1)
+game.display_board
+puts game.game_board.inspect
+# puts game.internal_board.inspect
+
 
 
--- ruby/connect_four/spec/connect_four_spec.rb
+++ ruby/connect_four/spec/connect_four_spec.rb
@@ -2,6 +2,35 @@ require './lib/connect_four'
 
 describe ConnectFour do
 
-  
+  describe '#make_move' do
+    subject(:game_move) { described_class.new }
+
+    context 'when player one chooses a column' do
+      it 'places the correct player piece of the bottom of the column' do
+        game_board = game_move.instance_variable_get(:@game_board)
+        player_1 = 1
+        col = 2
+
+        expect{ game_move.make_move(player_1, col) }.to change { game_board[col - 1][-1] }.from("O").to("1")
+      end
+    end
+
+    context 'when player one and then player 2 make a move in the same column' do
+      it 'places player 2 piece on top of player 1' do
+        game_board = game_move.instance_variable_get(:@game_board)
+
+        player_1 = 1
+        player_2 = 2
+        col = 2 
+
+        game_move.make_move(player_1,col)
+
+        expect { game_move.make_move(player_2,col) }.to change { game_board[col - 1][-2..-1] }.from(["O","1"]).to(["2","1"])
+      end
+    end
+
+    
+    
+  end
 
 end
