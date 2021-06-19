8367f20cb5571e5e986801b6e421e690d539b28d
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-06-18 23:29:48 -0400

    write node_in_path? for GamePiece class

--- ruby/chess/lib/gamepiece.rb
+++ ruby/chess/lib/gamepiece.rb
@@ -7,4 +7,15 @@ class GamePiece
 
   def valid_move?(new_pos, board)
   end
+
+  def get_path(start_pos, end_pos, board)
+  end
+
+  def node_in_path?(start_pos, end_pos)
+    path = get_path(start_pos, end_pos, board)
+
+    return false if path.length < 1
+    return false if path.all? {|node| node.piece.nil?}
+    true
+  end
 end
