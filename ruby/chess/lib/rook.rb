7f28394e2d84490f83fe9f02d8614cba99f8f5c0
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-06-19 18:02:01 -0400

    remove comments in rook.rb

--- ruby/chess/lib/rook.rb
+++ ruby/chess/lib/rook.rb
@@ -12,41 +12,4 @@ class Rook < GamePiece
     return false if piece_in_path?(@pos, new_pos, board)
     true
   end
-
-  # def get_path(start_pos, end_pos, board)
-  #   path = []
-  #   x1 = start_pos[0]
-  #   y1 = start_pos[1]
-  #   x2 = end_pos[0]
-  #   y2 = end_pos[1]
-
-  #   if x2 == x1
-  #     if y2 > y1
-  #       until y1 >= y2 - 1
-  #         y1 += 1
-  #         path.push(board.find_node([x1, y1]))
-  #       end 
-  #     elsif y2 < y1
-  #       until y2 >= y1 - 1
-  #         y2 += 1
-  #         path.push(board.find_node([x1, y2]))
-  #       end
-  #     end
-
-  #   elsif y2 == y1
-  #     if x2 > x1
-  #       until x1 >= x2 - 1
-  #         x1 += 1
-  #         path.push(board.find_node([x1, y1]))
-  #       end 
-  #     elsif x2 < x1
-  #       until x2 >= x1 - 1
-  #         x2 += 1
-  #         path.push(board.find_node([x2, y1]))
-  #       end
-  #     end
-  #   end
-  #   path
-  # end
-
 end
