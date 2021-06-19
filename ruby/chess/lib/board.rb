20910b18330af622c2d309c0674aff6f5f602694
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-06-19 16:18:11 -0400

    write is_linear? in Board, create board_spec.rb

--- ruby/chess/lib/board.rb
+++ ruby/chess/lib/board.rb
@@ -18,6 +18,16 @@ class Board
     true
   end
 
+  def is_linear?(start_pos, end_pos)
+    x1 = start_pos[0]
+    y1 = start_pos[1]
+    x2 = end_pos[0]
+    y2 = end_pos[1]
+
+    return false if x2 != x1 && y2 != y1 
+    true
+  end
+
   private
   def create_board
     a = Array.new(8){|i| i}
