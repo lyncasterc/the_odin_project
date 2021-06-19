274cb89770a0baa9c72623179bc621401b1bca74
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-06-19 17:48:07 -0400

    remove empty get_path in GamePiece

--- ruby/chess/lib/gamepiece.rb
+++ ruby/chess/lib/gamepiece.rb
@@ -8,10 +8,8 @@ class GamePiece
   def valid_move?(new_pos, board)
   end
 
-  def get_path(start_pos, end_pos, board)
-  end
-
   def piece_in_path?(start_pos, end_pos, board)
+
     path = get_path(start_pos, end_pos, board)
 
     return false if path.length < 1
