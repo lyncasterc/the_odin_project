8877cbe7a5e2bbd5a9b44309891f7ba27e734583
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-06-20 15:34:51 -0400

    create Bishop class, write valid_move?

---
+++ ruby/chess/lib/bishop.rb
@@ -0,0 +1,15 @@
+require_relative './gamepiece'
+
+class Bishop < GamePiece
+  def initialize(pos = nil, color = nil)
+    super(pos, color)
+  end
+
+  def valid_move?(new_pos, board)
+    return false if board.off_board?(new_pos)
+    return false if !board.is_diagonal?(@pos, new_pos)
+    return false if friendly_piece?(new_pos, board)
+    return false if piece_in_path?(@pos, new_pos, board)
+    true
+  end
+end
