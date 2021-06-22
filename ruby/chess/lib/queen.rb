5c1ec5767d8c06b6487fb700e591cc66c133990b
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-06-21 23:56:00 -0400

    add check for is_linear?, is_diagonal in Queen

--- ruby/chess/lib/queen.rb
+++ ruby/chess/lib/queen.rb
@@ -7,6 +7,7 @@ class Queen < GamePiece
 
   def valid_move?(new_pos, board)
     return false if board.off_board?(new_pos)
+    return false if !board.is_linear?(@pos, new_pos) || !board.is_diagonal?(@pos, new_pos)
     return false if friendly_piece?(new_pos, board)
     return false if piece_in_path?(@pos, new_pos, board)
     true
