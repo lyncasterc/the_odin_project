d9826f03b81cf515704b4427f9fc4cb32a2244d7
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-06-22 21:43:48 -0400

    create Knight class, valid_move?

---
+++ ruby/chess/lib/knight.rb
@@ -0,0 +1,25 @@
+require_relative './gamepiece'
+
+class Knight < GamePiece
+  def initialize(pos = nil, color = nil)
+    super(pos, color)
+  end
+
+  def valid_move?(new_pos, board)
+    x1 = @pos[0]
+    y1 = @pos[1]
+    x2 = new_pos[0]
+    y2 = new_pos[1]
+
+    return false if board.off_board?(new_pos)
+    return false if friendly_piece?(new_pos, board)
+
+    if (x2 - x1).abs() == 2 && (y2 - y1).abs() == 1
+      return true
+    elsif (y2 - y1).abs() == 2 && (x2 - x1).abs() == 1
+      return true
+    end
+
+    false
+  end
+end
