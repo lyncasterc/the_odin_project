2e8cb63a43aa57bc2b991d25b3a15e69c797648d
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-06-22 21:22:09 -0400

    refactor and test Queen valid_move?

--- ruby/chess/spec/queen_spec.rb
+++ ruby/chess/spec/queen_spec.rb
@@ -6,8 +6,42 @@ describe Queen do
   let(:chess_board) { Board.new }
 
   describe '#valid_move?' do
-    subject(:queen_move) { described_class.new }
+    subject(:queen_move) { described_class.new([3,3], 'white') }
 
-    
+    context 'when new_pos is not horizontal/vertical or diagonal' do
+      it 'returns false' do
+        new_pos = [4,5]
+
+        expect(queen_move.valid_move?(new_pos, chess_board)).to be false
+      end
+    end
+
+    context 'when new_pos is horizontal/vertical and valid' do
+      it 'returns true' do
+        new_pos = [3,5]
+
+        expect(queen_move.valid_move?(new_pos, chess_board)).to be true        
+      end
+
+      it 'works with other valid horizontal/vertical moves' do
+        new_pos = [2,3]
+
+        expect(queen_move.valid_move?(new_pos, chess_board)).to be true 
+      end
+    end
+
+    context 'when new_pos is diagonal and valid' do
+      it 'returns true' do
+        new_pos = [1,1]
+
+        expect(queen_move.valid_move?(new_pos, chess_board)).to be true 
+      end
+
+      it 'works with other diagonal moves' do
+        new_pos = [4,2]
+
+        expect(queen_move.valid_move?(new_pos, chess_board)).to be true
+      end
+    end
   end
 end
